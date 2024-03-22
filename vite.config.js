692732d98841a6cc3997bcc075fe4fff14a0ecfc
朱育辰 <33929606+oldpig123@users.noreply.github.com>
2024-03-22 17:26:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/Practicum_of_Attacking_and_Defense_of_Network_Security_webpage",
+  base: "/Practicum_of_Attacking_and_Defense_of_Network_Security_webpage",
   plugins: [react()],
 })
